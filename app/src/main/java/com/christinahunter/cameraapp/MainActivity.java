9b428eb7d7a7be3bcb7e27d0a4a25cfdc3bd65ca
christinahunter <frijole16@gmail.com>
2019-07-08 11:44:56 -0700

    Camera App Second Commit

--- app/src/main/java/com/christinahunter/cameraapp/MainActivity.java
+++ app/src/main/java/com/christinahunter/cameraapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,6 +15,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -74,9 +76,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            mImageView.setImageURI(mCurrentPhotoPath);
-
-
+            galleryAddPic();
+            setPic();
         }
     }
 
@@ -95,15 +96,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = Uri.fromFile(image);
-        galleryAddPic();
         return image;
     }
 
     private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath.getPath());
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
+        try {
+            MediaStore.Images.Media.insertImage(getContentResolver(), mCurrentPhotoPath.getPath(), "Foo", "Bar");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath.getPath(), bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath.getPath(), bmOptions);
+        mImageView.setImageBitmap(bitmap);
+    }
+
+
 }
